2119edfe04a5eed4d6843938b49996ab113ef698
alexochirov <vergil.009@mail.ru>
2019-06-19 09:04:34 +0300

    portfolio new

---
+++ Portfolio/README.md
@@ -0,0 +1,4 @@
+"npm i"
+Then for local development
+"npm start"
+or "npm run minify" for minify project
---
+++ Portfolio/app/js/app.js
@@ -0,0 +1,27 @@
+;
+
+
+(function($) {
+  "use strict";
+  $(function() {
+
+
+
+    $(document).ready(function() {
+      $(".header__link").on("click", function(event) {
+        event.preventDefault();
+
+        var id = $(this).attr('href'),
+
+          top = $(id).offset().top;
+
+        $('body,html').animate({
+          scrollTop: top
+        }, 300);
+      });
+    });
+
+
+
+  });
+})(jQuery);
