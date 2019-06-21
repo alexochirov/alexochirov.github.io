2adb7b456bac1ede38b66f147fec94d220815954
alexochirov <vergil.009@mail.ru>
2019-06-21 08:58:01 +0300

    pixel perfect

--- Best/csssr/dist/js/app.js
+++ Best/csssr/dist/js/app.js
@@ -1,1 +1,116 @@
-"use strict";!function(h){h(function(){var n=h('<div class="fake">').appendTo("body");function t(t){n.text(t.text()).width(t.width()).css("font-size",t.css("font-size")).css("font-family",t.css("font-family")).css("line-height",t.css("line-height"))}function e(t){var e=t.val();n.text(e);var i=t.css("line-height").slice(0,-2),s=Math.ceil(n.height()/i);t.height(s*i)}h(".paper__textarea").on({input:function(){e(h(this))},focus:function(){t(h(this))},keypress:function(t){13==t.which&&t.preventDefault()}}),h(document).ready(function(){o(),r(),t(h(".paper__textarea")),setTimeout(function(){e(h(".paper__textarea"))},1e3)});var s,a,l,c,i=(s=function(){o(),r(),t(h(".paper__textarea")),e(h(".paper__textarea"))},a=150,function(){var t=this,e=arguments,i=l&&!c;clearTimeout(c),c=setTimeout(function(){c=null,l||s.apply(t,e)},a),i&&s.apply(t,e)});function o(){h(".lore").each(function(){var t=h(this).find(".lore__block").map(function(){return h(this).height()}).get(),e=Math.max.apply(null,t)+60;h(this).height(e)})}function r(){h(".lore").each(function(){var t,e=h(this).width()-5;t=h(this).find(".lore__block.js-active").hasClass("lore__block_last")?e:h(".lore__block.js-active").css("left"),h(this).find(".lore__triangle").css("left",t)})}window.addEventListener("resize",i),h(".lore__block").click(function(){var t=h(this).css("left"),e=h(this).closest(".lore").width()-5;h(this).hasClass("lore__block_last")?h(this).closest(".lore").find(".lore__triangle").css("left",e):h(this).closest(".lore").find(".lore__triangle").css("left",t),h(".lore__block.js-active").removeClass("js-active"),h(this).addClass("js-active")})})}(jQuery);
+"use strict";
+
+;
+
+(function ($) {
+  "use strict";
+
+  $(function () {
+    //begin of .paper__textarea helper function
+    var $span = $('<div class="fake">').appendTo('body');
+
+    function initDivForCalculateHeightOfTextarea(textarea) {
+      $span.text(textarea.text()).width(textarea.width()).css('font-size', textarea.css('font-size')).css('font-family', textarea.css('font-family')).css('line-height', textarea.css('line-height'));
+    }
+
+    $('.paper__textarea').on({
+      input: function input() {
+        setHeightOfTextarea($(this));
+      },
+      focus: function focus() {
+        initDivForCalculateHeightOfTextarea($(this));
+      },
+      keypress: function keypress(e) {
+        if (e.which == 13) e.preventDefault();
+      }
+    });
+
+    function setHeightOfTextarea(area) {
+      var text = area.val();
+      $span.text(text);
+      var lineHeight = area.css('line-height').slice(0, -2);
+      var numOfRows = Math.ceil($span.height() / lineHeight);
+      area.height(numOfRows * lineHeight);
+    } //end of .paper__textarea helper function
+    //begin of debounce function
+
+
+    function debounce(func, wait, immediate) {
+      var timeout;
+      return function () {
+        var context = this,
+            args = arguments;
+
+        var later = function later() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    }
+
+    ; //end of debounce function
+    //begin of .lore slider activity
+
+    $(document).ready(function () {
+      createHeightOfSlider();
+      positioningOfTrianle();
+      initDivForCalculateHeightOfTextarea($('.paper__textarea'));
+      setTimeout(function () {
+        setHeightOfTextarea($('.paper__textarea'));
+      }, 1000);
+    });
+    var debounceOfSlider = debounce(function () {
+      createHeightOfSlider();
+      positioningOfTrianle();
+      initDivForCalculateHeightOfTextarea($('.paper__textarea'));
+      setHeightOfTextarea($('.paper__textarea'));
+    }, 150);
+    window.addEventListener('resize', debounceOfSlider);
+
+    function createHeightOfSlider() {
+      var heightAdded = 60;
+      $('.lore').each(function () {
+        var $currentBlocks = $(this).find('.lore__block');
+        var heights = $currentBlocks.map(function () {
+          return $(this).height();
+        }).get();
+        var maxHeight = Math.max.apply(null, heights) + heightAdded;
+        $(this).height(maxHeight);
+      });
+    }
+
+    function positioningOfTrianle() {
+      $('.lore').each(function () {
+        var leftOffsetOfCurrentBlock;
+        var rightOffsetOfCurrentBlock = $(this).width() - 5;
+
+        if ($(this).find('.lore__block.js-active').hasClass('lore__block_last')) {
+          leftOffsetOfCurrentBlock = rightOffsetOfCurrentBlock;
+        } else {
+          leftOffsetOfCurrentBlock = $('.lore__block.js-active').css('left');
+        }
+
+        $(this).find('.lore__triangle').css('left', leftOffsetOfCurrentBlock);
+      });
+    }
+
+    $('.lore__block').click(function () {
+      var leftOffsetOfCurrentBlock = $(this).css('left');
+      var rightOffsetOfCurrentBlock = $(this).closest('.lore').width() - 5;
+
+      if ($(this).hasClass('lore__block_last')) {
+        $(this).closest('.lore').find('.lore__triangle').css('left', rightOffsetOfCurrentBlock);
+      } else {
+        $(this).closest('.lore').find('.lore__triangle').css('left', leftOffsetOfCurrentBlock);
+      }
+
+      $('.lore__block.js-active').removeClass('js-active');
+      $(this).addClass('js-active');
+    }); //end of .lore slider activity
+  });
+})(jQuery);
