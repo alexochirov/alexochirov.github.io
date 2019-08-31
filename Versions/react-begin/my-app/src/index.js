e9b8034fa39912d36c0ff440a4c6120cf7548a7b
alexochirov <vergil.009@mail.ru>
2019-08-31 15:40:59 +0300

    freact calc

--- Versions/react-begin/my-app/src/index.js
+++ Versions/react-begin/my-app/src/index.js
@@ -4,37 +4,43 @@ import "./style.scss";
 import * as serviceWorker from "./serviceWorker";
 
 class Calc extends React.Component {
-  state = {
-    total: null,
-    next: null,
-    operation: null
-  };
-  handleClick(){
-    
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null
+    };
+  }
+
+  handleClick(value) {
+    let prevValue = this.state.total;
     this.setState({
-      total: 2
+      total: (prevValue += value).toString()
     });
-    console.log(this.state.buttonInfo);
-  };
+  }
 
   render() {
     let numsButtons = [];
     for (var i = 0; i < 10; i++) {
       numsButtons.push(
-        <Button num={i} key={i} handleClick={this.handleClick()} />
+        <Button num={i} key={i} handleClick={this.handleClick} />
       );
     }
 
     return (
       <div className="calc">
-        <div className="calc__main" />
+        <div className="calc__main">{this.state.total}</div>
         <div className="calc__panel">
           {numsButtons}
-          <button className="calc__button">.</button>
+
+            <Button num="." key="." handleClick={this.handleClick} />
           <button className="calc__button calc__button_sign">+</button>
           <button className="calc__button calc__button_sign">-</button>
           <button className="calc__button calc__button_sign">×</button>
           <button className="calc__button calc__button_sign">÷</button>
+          <button className="calc__button calc__button_sign">=</button>
         </div>
       </div>
     );
@@ -47,11 +53,15 @@ class Button extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick() {
-    this.props.handleClick();
+    this.props.handleClick(this.props.num);
   }
   render() {
     return (
-      <button className="calc__button" onClick={this.props.handleClick}>
+      <button
+        className="calc__button"
+        value={this.props.value}
+        onClick={this.handleClick}
+      >
         {this.props.num}
       </button>
     );
