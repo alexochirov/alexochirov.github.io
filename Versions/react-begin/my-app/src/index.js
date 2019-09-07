84e14c7fa08c0ca84215d03bac6f5e2fc896dbdb
alexochirov <vergil.009@mail.ru>
2019-09-07 11:43:59 +0300

    react

--- Versions/react-begin/my-app/src/index.js
+++ Versions/react-begin/my-app/src/index.js
@@ -6,41 +6,72 @@ import * as serviceWorker from "./serviceWorker";
 class Calc extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+    this.handleNum = this.handleNum.bind(this);
     this.state = {
-      total: null,
-      next: null,
-      operation: null
+      operation: null,
+      prev: 0,
+      currView: 0,
+      total: 0
     };
   }
 
-  handleClick(value) {
-    let prevValue = this.state.total;
-    this.setState({
-      total: (prevValue += value).toString()
-    });
+  handleNum(value) {
+    if (value === "+") {
+      this.setState({
+        operation: value,
+        prev: parseFloat(this.state.currView),
+        
+      });
+    } else if (value === "ac") {
+      this.setState({
+        operation: null,
+        prev: 0,
+        currView: 0
+      });
+    } else if (value === "=") {
+    } else {
+      let prevValue;
+      if (this.state.operation === null) {
+        prevValue = this.state.currView;
+      } else {
+        prevValue = this.state.total;
+      }
+      console.log(value);
+      let input;
+      let currValue;
+      if (prevValue.toString().indexOf(".") === -1) {
+        currValue = (prevValue + value).toString();
+      } else {
+        input = (prevValue + value).toString().split(".");
+        currValue = input.shift() + "." + input.join("");
+      }
+
+      this.setState({
+        currView: currValue
+      });
+    }
   }
 
   render() {
     let numsButtons = [];
     for (var i = 0; i < 10; i++) {
-      numsButtons.push(
-        <Button num={i} key={i} handleClick={this.handleClick} />
-      );
+      numsButtons.push(<Button num={i} key={i} handleClick={this.handleNum} />);
     }
-
+    console.log(this.state);
     return (
       <div className="calc">
-        <div className="calc__main">{this.state.total}</div>
+        <div className="calc__main">{Number(this.state.currView)}</div>
         <div className="calc__panel">
           {numsButtons}
 
-            <Button num="." key="." handleClick={this.handleClick} />
+          <Button num="." handleClick={this.handleNum} />
+          <Button num="+" isOperation={true} handleClick={this.handleNum} />
           <button className="calc__button calc__button_sign">+</button>
           <button className="calc__button calc__button_sign">-</button>
           <button className="calc__button calc__button_sign">×</button>
           <button className="calc__button calc__button_sign">÷</button>
-          <button className="calc__button calc__button_sign">=</button>
+          <Button num="=" isOperation={true} handleClick={this.handleNum} />
+          <Button num="ac" isOperation={true} handleClick={this.handleNum} />
         </div>
       </div>
     );
@@ -56,9 +87,13 @@ class Button extends React.Component {
     this.props.handleClick(this.props.num);
   }
   render() {
+    const classes =
+      this.props.isOperation === true
+        ? "calc__button calc__button_sign"
+        : "calc__button ";
     return (
       <button
-        className="calc__button"
+        className={classes}
         value={this.props.value}
         onClick={this.handleClick}
       >
