cbb9f3091970993830f32ea079070aaa5ee07a2c
alexochirov <vergil.009@mail.ru>
2025-09-13 07:14:20 +0300

    Delete Portfolio directory

--- Portfolio/README.md
+++
@@ -1,4 +0,0 @@
-"npm i"
-Then for local development
-"npm start"
-or "npm run minify" for minify project
--- Portfolio/dist/js/app.js
+++
@@ -1,1 +0,0 @@
-"use strict";!function(o){o(function(){o(document).ready(function(){o(".header__link").on("click",function(t){t.preventDefault();var n=o(this).attr("href"),e=o(n).offset().top;o("body,html").animate({scrollTop:e},300)})})})}(jQuery);
